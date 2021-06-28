a806255b17ec8f11f271db38173815d97c99f845
gajen <meetgajen@yahoo.com>
2021-06-28 05:58:07 -0400

    commit tree 0dcab354b5658292db3aff1e2d2960d56044f6e5

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'PNBpo9v9PR5ZCrGihAZ3VJ8tKGhG2fjp';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+
+
+class LatestMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state ={
+            reviews: []
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(resp => this.setState({
+            reviews: resp.results
+        }))
+    }
+
+
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        );
+    }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'PNBpo9v9PR5ZCrGihAZ3VJ8tKGhG2fjp';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer  extends Component {
+    constructor(){
+        super()
+        this.state ={
+            searchTerm: '', reviews: []
+        }
+    }
+
+    submitEvent(event) {
+        event.preventDefault();
+        fetch(URL.concat(this.state.searchTerm))
+            .then(resp => resp.json())
+            .then(resp => console.log(resp));
+      }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+        <h3>The Searched Movie Reviews:</h3>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+        );
+    }
+}
+
+export default SearchableMovieReviewsContainer ;
